d04b44e1a26ad6c7cfeae316e064d0f2e2f1a826
havard.stranden <havard.stranden@907efb9a-82b2-11dd-9205-735403a6da14>
2008-09-15 00:31:48 +0000

    Missing files from previous commit (tool error)
    git-svn-id: https://strongbind.googlecode.com/svn/trunk@6 907efb9a-82b2-11dd-9205-735403a6da14

--- src/OX.DataBinding.TestApp/Properties/Settings.Designer.cs
+++ src/OX.DataBinding.TestApp/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace OX.DataBinding.TestApp.Properties {
+namespace OX.Strongbind.TestApp.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
